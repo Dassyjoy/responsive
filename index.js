00806d3666005c463fa7450064e57e6f135da053
Dassyjoy <114305536+Dassyjoy@users.noreply.github.com>
2023-03-01 05:17:42 -0800

    first command

---
+++ index.js
@@ -0,0 +1,8 @@
+
+slider.classList.add("moveslider");
+formSection.classList.add("form-section-move");
+
+login.addEventListener("click", () => {
+slider.classList.remove("moveslider");
+formSection.classList.remove("form-section-move");
+});
